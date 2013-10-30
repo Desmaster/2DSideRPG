9e3c69ceb17ccc41c210685f8ec0e583a16ef1c1
Timon de Groot <tdegroot96@gmail.com>
2013-10-30 11:54:12 +0100

    Fixed Collision

--- src/core/game/Game.java
+++ src/core/game/Game.java
@@ -22,7 +22,7 @@ public class Game extends BasicGame {
 	public void init(GameContainer gameContainer) throws SlickException {
 		screen = new Screen(gameContainer.getGraphics());
 		player = new Player(0, 0);
-		level = new Level("/res/level.tmx");
+		level = new Level("/res/naamloos1.tmx");
 		level.add(player);
 	}
 
